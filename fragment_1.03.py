c281e90593e8fe17e913e2c93b80a1140282445d
Tibaeriu <tiberiu.stanciu@gmail.com>
2020-05-13 20:09:01 +0300

    20200513.2

--- fragment_1.03.py
+++ fragment_1.03.py
@@ -1,3 +1,13 @@
 lista = ['Florin', 'Catalin', 'Marin', 'Ionel']
 for i in range (len(lista)):
     print(f"Elementul de pe pozitia {i} este {lista[i]}.")
+
+#list.append - adaugare elemente
+#list.extend - extindere elemente
+#list.insert - inserare element
+#list.index - index element
+#list.copy - copie lista
+#list.clear - curatare lista
+#list.count - nr de aparitii element
+#list.reverse - inversare elemente
+#list.pop - stergere element
